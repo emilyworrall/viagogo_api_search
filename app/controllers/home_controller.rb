c4eea1ac78ce0a47750f51bdc73fbada3ffdf73d
Emily Worrall <worrall.emily@gmail.com>
2016-02-09 15:01:20 +0000

    Connected to viagogo api and return json of search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,17 @@
+require 'gogokit'
+require 'json'
+
 class HomeController < ApplicationController
+
+  def index
+  end
+
+  def show
+    api = GogoKit::Client.new(client_id: ENV['VIAGOGO_ID'],
+                              client_secret: ENV['VIAGOGO_SECRET'])
+    token = api.get_client_access_token
+    api.access_token = token.access_token
+    search_results = api.search(params[:search_term])
+    render json: search_results
+  end
 end
