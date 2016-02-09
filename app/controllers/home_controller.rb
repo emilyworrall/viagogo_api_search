ec3b7fb892bd6c0ea5186575ae2c11edadc2b68e
Emily Worrall <worrall.emily@gmail.com>
2016-02-09 15:08:37 +0000

    Can display search results on results page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,20 @@ class HomeController < ApplicationController
     token = api.get_client_access_token
     api.access_token = token.access_token
     search_results = api.search(params[:search_term])
-    render json: search_results
+
+    @category = []
+    i = 0
+    while i < 10 do
+      if search_results.items[i] == nil
+        break
+      end
+      i += 1
+      next if search_results.items[i - 1].links['searchresult:category'] == nil
+      @category << api.object_from_response(GogoKit::Category,
+                                            GogoKit::CategoryRepresenter,
+                                            :get,
+                                            search_results.items[i - 1].links['searchresult:category'].href,
+                                            nil)
+    end
   end
 end
